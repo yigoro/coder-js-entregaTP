f3dcbb9f650d4d2f33bd3fbafcd9a31821dce5c0
yigoro <seba.bertolotti@gmail.com>
2024-10-30 10:06:18 -0300

    Primer commit de entrega de proyecto

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const app = () => {
+    let opcion = prompt("hola")
+}
+
+let resp = app ()
+
+const guardarDatos = () => {
+    //Esta funcion va a tomar los datos ingresados en el formulario y los guarda en un array
+}
+
+const mostrarPromedios = () => {
+    //Esta funcion recorre el array y muestra el promedio general de todos los alumnos
+}
+
+const mostrarAlumnos = () => {
+    //Esta funcion muestra por pantalla el listado de alumnos ingresados
+}
